b6b7be64b7d53057f358002d63ccb71c99da6090
eddyxu <eddyxu@1509d998-9a31-0410-928a-b91e0fff2e5a>
2007-05-29 17:00:34 +0000

    + use PortsHelper to set new launchd.plist

--- trunk/PortsHelper.c
+++ trunk/PortsHelper.c
@@ -12,8 +12,57 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <syslog.h>
 
-#define PN_PORT_PATH "/opt/local/bin/port"
+#include "PNDefines.h"
+
+void WritePropertyListToFile( CFPropertyListRef propertyList,
+            CFURLRef fileURL ) {
+   CFDataRef xmlData;
+   Boolean status;
+   SInt32 errorCode;
+
+   // Convert the property list into XML data.
+   xmlData = CFPropertyListCreateXMLData( kCFAllocatorDefault, propertyList );
+
+   // Write the XML data to the file.
+   status = CFURLWriteDataAndPropertiesToResource (
+               fileURL,                  // URL to use
+               xmlData,                  // data to write
+               NULL,
+               &errorCode);
+
+   CFRelease(xmlData);
+}
+
+CFPropertyListRef CreatePropertyListFromFile( CFURLRef fileURL ) {
+
+   CFPropertyListRef propertyList;
+   CFStringRef       errorString;
+   CFDataRef         resourceData;
+   Boolean           status;
+   SInt32            errorCode;
+
+   // Read the XML file.
+   status = CFURLCreateDataAndPropertiesFromResource(
+               kCFAllocatorDefault,
+               fileURL,
+               &resourceData,            // place to put file data
+               NULL,
+               NULL,
+               &errorCode);
+
+   // Reconstitute the dictionary using the XML data.
+   propertyList = CFPropertyListCreateFromXMLData( kCFAllocatorDefault,
+               resourceData,
+               kCFPropertyListImmutable,
+               &errorString);
+
+   CFRelease( resourceData );
+
+   return propertyList;
+
+}
 
 int pn_is_ports_installed(void)
 {
@@ -25,7 +74,45 @@ int pn_sync_ports(void)
 	if(!pn_is_ports_installed())
 		return 1;
 		
+	syslog(LOG_INFO ,"Run port selfupdate");
 	return system("/opt/local/bin/port selfupdate");
+}
+
+CFDictionaryRef pn_create_launchd_plist(int interval){
+	CFMutableDictionaryRef dict;
+	CFNumberRef num;
+	
+	int seconds = interval * 60; // translate to seconds
+	
+	// Create a dictionary that will hold the data.
+	
+	dict = CFDictionaryCreateMutable( kCFAllocatorDefault,
+									  0,
+									  &kCFTypeDictionaryKeyCallBacks,
+									  &kCFTypeDictionaryValueCallBacks );
+									  
+	CFDictionarySetValue(dict, CFSTR("Label"), CFSTR(PN_APP_ID));
+	CFDictionarySetValue(dict, CFSTR("Program"), CFSTR(PN_HELPER_PATH));
+	CFDictionarySetValue(dict, CFSTR("ServiceDescription"),
+			CFSTR("Automatic update macports"));
+	CFDictionarySetValue(dict, CFSTR("StandardErrorPath"), 
+			CFSTR("/var/log/portsnotifier.log"));
+			
+	num = CFNumberCreate( kCFAllocatorDefault,
+            kCFNumberIntType,
+            &seconds );
+	CFDictionarySetValue( dict, CFSTR("StartInterval"), num );
+	CFRelease( num );
+   
+	int timeout = 125;
+	num = CFNumberCreate( kCFAllocatorDefault,
+            kCFNumberIntType,
+            &timeout );
+	CFDictionarySetValue( dict, CFSTR("TimeOut"), num );
+	CFRelease( num );
+   
+	CFDictionarySetValue( dict, CFSTR("UserName"), CFSTR("root"));
+	return dict;
 }
 
 int pn_write_plist(int interval)
@@ -33,7 +120,18 @@ int pn_write_plist(int interval)
 	if(interval < 0) 
 		return -1;
 		
-	//CFStringRef names[2];
+	syslog(LOG_INFO, "Modified interval to: %d", interval);
+	
+	CFPropertyListRef propertyList;
+	CFURLRef fileURL;
+ 
+	fileURL = CFURLCreateWithFileSystemPath( kCFAllocatorDefault,
+               CFSTR(PN_LAUNCHD_PLIST),       // file path name
+               kCFURLPOSIXPathStyle,    // interpret as POSIX path
+               false ); 
+	propertyList = pn_create_launchd_plist(interval);
+	WritePropertyListToFile( propertyList, fileURL );
+	CFRelease(propertyList);
 	return 0;
 }
 
@@ -49,6 +147,8 @@ void pn_usage(void)
 
 int main(int argc, char * const argv[])
 {
+	
+	openlog("PortsHelper", (LOG_CONS|LOG_PERROR|LOG_PID), LOG_DAEMON);
 	int ch, ret;
 	
 	while((ch = getopt(argc, argv, "hi:")) != -1){
@@ -72,6 +172,9 @@ int main(int argc, char * const argv[])
 	argc -= optind;
 	argv += optind;
 	
-	return pn_sync_ports();
+	
+	ret =  pn_sync_ports();
+	closelog();
+	return ret;
 }
 
